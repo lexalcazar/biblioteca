da79aef2533d050ae6a1c846de1aa3df3b354279
Alexander Alcázar <aalcazarprogramacion@gmail.com>
2026-01-16 15:16:11 +0100

    conexion a bd y primera lista de libros funcionando

--- prestamos/views.py
+++ prestamos/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 #Pagina de inicio de la aplicacion de prestamos
 def index(request):
     return render(request, 'prestamos/index.html')
+# listado de libros disponibles
+def lista_libros(request):
+    return render(request, 'prestamos/lista_libros.html')
+# detalle de un libro
+def detalle_libro(request, libro_titulo):
+    return render(request, 'prestamos/detalle_libro.html', {'libro_titulo': libro_titulo})
